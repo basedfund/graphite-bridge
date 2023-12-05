b5217f6d9573f0657a95d4bb3f126f33867efe62
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2023-12-05 12:51:33 -0600

    test commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+#     long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Test'
+LONG_DESCRIPTION = 'Test'
+
+# Setting up
+setup(
+    name="vidstream",
+    version=VERSION,
+    author="Jonathan Wengel",
+    author_email="<jonthan.anodaram@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'graphite', 'trading', 'cryptocurrency'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
