819e92b4d29b58750e5faf3c1d3d62eaa2cc08fd
ChainDev931105 <kaitoyamamoto931105@outlook.com>
2024-01-03 15:13:49 -0600

    v0.2.2: feat authorization

--- graphite_bridge/server.py
+++ graphite_bridge/server.py
@@ -15,6 +15,11 @@ class GraphiteServerConnector:
             self.__on_message = on_message
 
     def connect(self):
+        data = { 'email': self.__email, 'password': self.__password }
+        (success, response) = self.__send_request('POST', '/api/auth/login', data, True)
+        if not success:
+            return False
+
         def run(*args):
             websocket_url = self.__server_url.replace("http", "ws") + "/client_ws"
             self.__ws = websocket.WebSocketApp(websocket_url, on_message=self.on_message, on_open=self.on_open, on_close=self.on_close)
@@ -43,16 +48,32 @@ class GraphiteServerConnector:
         self.__connected = False
 
     def get_client_config(self):
-        response = requests.get(self.__server_url + "/api/client/config?strategyId=" + self.__strategy_id)
-        if response.status_code != 200:
-            return (False, response.reason)
-        response_json = response.json()
-        return (response_json['success'], response_json['data'])
+        return self.__send_request('GET', '/api/client/config?strategyId=' + self.__strategy_id)
 
     def upload_state(self, state):
         data = { 'strategyId': self.__strategy_id, 'state': state }
-        response = requests.post(self.__server_url + "/api/client/upload", json=data)
+        return self.__send_request('POST', '/api/client/upload', data)
+    
+    def __send_request(self, method, endpoint, data=None, for_login=False):
+        url = self.__server_url + endpoint
+        response = None
+
+        if method == 'GET':
+            response = requests.get(url, headers={'Authorization': 'Bearer ' + self.__auth_token})
+        elif method == 'POST':
+            if for_login:
+                response = requests.post(url, json=data)
+            else:
+                response = requests.post(url, headers={'Authorization': 'Bearer ' + self.__auth_token}, json=data)
+
         if response.status_code != 200:
+            print('Request failed. Reason:', response.reason, response.json())
             return (False, response.reason)
         response_json = response.json()
+
+        if for_login:
+            self.__auth_token = response_json['token']
+            return (response_json['success'], response_json)
+
         return (response_json['success'], response_json['data'])
+    
